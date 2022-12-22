667e29d5aba66a6624e26f48334f98c92834397f
bnbailey-psl <bnbailey@ucdavis.edu>
2022-12-22 09:04:08 -0800

    * Added GitHub Actions workflows to automatically run utilities/run_samples.sh on push or pull requests. * Fixed issue on Windows in core/lib/libpng/CMakeLists.txt file where it could not find the zlib dll. * Fixed issue on Windows in project CMakeLists.txt files that would cause a build error because 'cmake_minimum_required' was not specified directly. * Modified core/CMake_project.txt file to be sure that it puts .dll's in the lib directory and not in a sub-directory based on the build type * utilities/run_samples.sh script modified such that it can take an argument '--visbuildonly' to skip runs for visualizer-based projects, and so that this script now works on Windows
    *Visualizer*
    - Modified CMakeLists.txt to only copy glfw3.dll file if it exists (Windows only).

--- utilities/run_samples.sh
+++ utilities/run_samples.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 SAMPLES=("context_selftest" "visualizer_selftest" "radiation_selftest" "energybalance_selftest" "solarposition_selftest" "stomatalconductance_selftest" "photosynthesis_selftest" "weberpenntree_selftest" "lidar_selftest" "aeriallidar_selftest" "voxelintersection_selftest" "canopygenerator_selftest" "boundarylayerconductance_selftest" "tutorial0" "tutorial1" "tutorial2" "tutorial5")
-SAMPLES_NOGPU=("context_selftest" "solarposition_selftest" "stomatalconductance_selftest" "photosynthesis_selftest" "weberpenntree_selftest" "canopygenerator_selftest" "boundarylayerconductance_selftest" "tutorial0" "tutorial1" "tutorial2" "tutorial5")
+SAMPLES_NOGPU=("context_selftest" "visualizer_selftest" "solarposition_selftest" "stomatalconductance_selftest" "photosynthesis_selftest" "weberpenntree_selftest" "canopygenerator_selftest" "boundarylayerconductance_selftest" "tutorial0" "tutorial1" "tutorial2" "tutorial5")
 
 TEST_PLUGINS="energybalance lidar aeriallidar photosynthesis radiation solarposition stomatalconductance visualizer voxelintersection weberpenntree canopygenerator boundarylayerconductance"
 TEST_PLUGINS_NOGPU="photosynthesis solarposition stomatalconductance visualizer weberpenntree canopygenerator boundarylayerconductance"
 
 cd ../samples
+
+if [[ "${OSTYPE}" != "darwin"* ]] && [[ "${OSTYPE}" != "linux"* ]] || [[ "${OSTYPE}" != "msys"* ]];then
+  echo "UNSUPPORTED OPERATING SYSTEM"
+  exit 1
+fi
 
 while [ $# -gt 0 ]; do
   case $1 in
@@ -33,6 +38,10 @@ while [ $# -gt 0 ]; do
   --nogpu)
     SAMPLES=("${SAMPLES_NOGPU[@]}")
     TEST_PLUGINS=${TEST_PLUGINS_NOGPU}
+    ;;
+
+  --visbuildonly)
+    VISRUN="OFF"
     ;;
 
   esac
@@ -64,9 +73,10 @@ else
     cd build
 
     echo -ne "Building project creation script test..."
-    #if  cmake .. 2> /dev/null | grep -q 'Build files have been written to'  ;then
-    if ! cmake .. 2>/dev/null | grep -qi 'CMake Error'; then
-      #if ! cmake .. | grep -qi 'CMake Error'  ;then
+
+    cmake .. &>/dev/null
+
+    if (($? == 0)); then
       echo -e "\r\x1B[32mBuilding project creation script test...done.\x1B[39m"
     else
       echo -e "\r\x1B[31mBuilding project creation script test...failed.\x1B[39m"
@@ -75,7 +85,9 @@ else
 
     echo -ne "Compiling project creation script test..."
 
-    if make 2>/dev/null | grep -Fq '[100%]'; then
+    cmake --build ./ --target temp &>/dev/null
+
+    if (($? == 0)); then
       if [ -e "temp" ]; then
         echo -e "\r\x1B[32mCompiling project creation script test...done.\x1B[39m"
       else
@@ -89,7 +101,11 @@ else
 
     echo -ne "Running project creation script test..."
 
-    ./temp &>/dev/null
+    if [[ "${OSTYPE}" == "msys"* ]];then
+      ./temp.exe &>/dev/null
+    else
+      ./temp &>/dev/null
+    fi
 
     if (($? == 0)); then
       echo -e "\r\x1B[32mRunning project creation script test...passed.\x1B[39m"
@@ -111,11 +127,11 @@ for i in "${SAMPLES[@]}"; do
 
   if [ ! -e "${i}" ]; then
     echo "Sample ${i} does not exist."
-    exit 0
+    exit 1
   fi
   if [ ! -e "${i}/build" ]; then
     echo "Build directory does not exist for sample ${i}."
-    exit 0
+    exit 1
   fi
 
   cd "$i"/build
@@ -124,7 +140,9 @@ for i in "${SAMPLES[@]}"; do
 
   echo -ne "Building sample ${i}..."
 
-  if cmake .. 2>/dev/null | grep -q 'Build files have been written to'; then
+  cmake .. &>/dev/null
+
+  if (($? == 0)); then
     echo -e "\r\x1B[32mBuilding sample ${i}...done.\x1B[39m"
   else
     echo -e "\r\x1B[31mBuilding sample ${i}...failed.\x1B[39m"
@@ -136,7 +154,9 @@ for i in "${SAMPLES[@]}"; do
 
   echo -ne "Compiling sample ${i}..."
 
-  if make 2>/dev/null | grep -Fq '[100%]'; then
+  cmake --build ./ --target "${i}" &>/dev/null
+
+  if (($? == 0)); then
     if [ -e "${i}" ]; then
       echo -e "\r\x1B[32mCompiling sample ${i}...done.\x1B[39m"
     else
@@ -154,9 +174,21 @@ for i in "${SAMPLES[@]}"; do
     continue
   fi
 
+  if [ -n "${VISRUN}" ];then
+    if  grep -qi visualizer ../CMakeLists.txt ;then
+      echo "Skipping run for case ${i} because it uses the visualizer..."
+      cd ../..
+      continue
+    fi
+  fi
+
   echo -ne "Running sample ${i}..."
 
-  "./${i}" &>/dev/null
+  if [[ "${OSTYPE}" == "msys"* ]];then
+    "./${i}.exe" &>/dev/null
+  else
+    "./${i}" &>/dev/null
+  fi
 
   if (($? == 0)); then
     echo -e "\r\x1B[32mRunning sample ${i}...passed.\x1B[39m"
