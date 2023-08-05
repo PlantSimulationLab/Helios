745476e859f6f85fac4abb7c7d129d96d0816ed4
bnbailey-psl <bnbailey@ucdavis.edu>
2023-08-05 13:40:20 -0700

    [1.2.67] 2023-08-08
    * Changed core/CMake_project.txt to set CMAKE_RUNTIME_OUTPUT_DIRECTORY and CMAKE_LIBRARY_OUTPUT_DIRECTORY variables, which seems to resolve some intermittent issues with not finding .dll locations on Windows.
    
    *Aerial LiDAR*
    - Updated CMakeLists.txt to deprecate compute capability 3.5.
    
    *Energy Balance*
    - Updated CMakeLists.txt to deprecate compute capability 3.5.
    
    *LiDAR*
    - Updated CMakeLists.txt to deprecate compute capability 3.5.
    
    *Voxel Intersection*
    - Updated CMakeLists.txt to deprecate compute capability 3.5.

--- doc/html/search/pages_7.js
+++ doc/html/search/pages_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['helios_20documentation_20v1_2e2_2e66_0',['Helios Documentation v1.2.66',['../index.html',1,'']]]
+  ['helios_20documentation_20v1_2e2_2e67_0',['Helios Documentation v1.2.67',['../index.html',1,'']]]
 ];
